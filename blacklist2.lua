002aa05b4bd34317e1cbad37fc79ef308a67c9b6
V.Roy <andidiazas@gmail.com>
2026-09-08 03:37:44 +0200

    Update blacklist2.lua

--- blacklist2.lua
+++ blacklist2.lua
@@ -2,5 +2,6 @@ return {
     ["Hapy64534"] = true,
     ["666XxBlessedZiad666"] = true,
     ["mfaith139"] = true,
-    [""] = true,
+    ["Juniorpoppy12t"] = true,
+    ["VeneTrumStudios"] = true,
 }
